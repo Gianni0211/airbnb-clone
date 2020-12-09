32d68c76e0fb809175f6531113c5b3ac9273126b
Gianni Mondello <gianni.mondello@live.it>
2020-12-09 10:06:49 +0100

    fix photo carousel

--- airbnb/src/components/IndexCard.vue
+++ airbnb/src/components/IndexCard.vue
@@ -1,31 +1,29 @@
 <template>
-  <div>
-    <div class="mr-5 h-64 flex  items-center border-t">
-    <photo-carousel :images="location.images" />
-      <div class="self-start mt-10">
-        <p class="text-sm text-gray-400">{{ location.subtitle }}</p>
-        <h2 class="text-xl">{{ location.name }}</h2>
-        <hr class="w-10 mt-2" />
-        <p class="text-sm text-gray-400">{{ location.description }}</p>
-      </div>
-    </div>
-  </div>
+    <router-link to="{name: 'show'}">
+        <div class="mr-5 h-64 flex  items-center border-t">
+            <photo-carousel :images="location.images" @click.prevent />
+            <div class="self-start mt-10">
+                <p class="text-sm text-gray-400">{{ location.subtitle }}</p>
+                <h2 class="text-xl">{{ location.name }}</h2>
+                <hr class="w-10 mt-2" />
+                <p class="text-sm text-gray-400">{{ location.description }}</p>
+            </div>
+        </div>
+    </router-link>
 </template>
 
 <script>
-import PhotoCarousel from '../components/PhotoCarousel.vue';
+import PhotoCarousel from "../components/PhotoCarousel.vue";
 export default {
-  name: "IndexCard",
-  components: { PhotoCarousel },
-  props: {
-    location: {
-      type: Object,
-      required: true,
-    },
-  },
-  
+    name: "IndexCard",
+    components: { PhotoCarousel },
+    props: {
+        location: {
+            type: Object,
+            required: true
+        }
+    }
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
